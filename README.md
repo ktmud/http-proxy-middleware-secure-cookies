6743e10c497937b8ad518eeb596f56981d01edd6
Jesse Yang <jesse.yang@airbnb.com>
2020-08-25 14:38:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# http-proxy-middleware-secure-cookies
+
+Create a [http-proxy-middleware](https://www.npmjs.com/package/http-proxy-middleware) that securly make requests with cookies. Normally used in a [webpack-dev-server](https://www.npmjs.com/package/webpack-dev-server).
+
+## Problem Statement
+
+Imagine you have an API backend and a totally separate frontend. The dev environment for the backend is very complex to setup so sometimes you'd prefer to let local frontend dev server make direct requests to the production services or some shared environment.
+
+Now the tricky thing is the production service may require authentication. You local proxy has to bear the authentication headers or cookies in order to make requests to the API service. This package allows you to more easily manage and securely store the credentials needed for making these requests.
+
+## Usage
+
+In `webpack.config.js`, add `devServer` proxy rules like below:
+
+```ts
+const secureCookiesProxy = require('http-proxy-middleware-secure-cookies');
+
+module.exports = {
+  // ...
+  devServer: {
+    // ...
+    proxy: {
+      // use the default options to proxy /api/* to https://foo.example.com/api/*
+      '/api/*': secureCookiesProxy('https://api.example.com'),
+
+      // different endpoints share the same account
+      '/proxy/fiz': secureCookiesProxy({
+        target: 'https://bar.example.com',
+        keychainAccount: 'example.com',
+      });
+      '/proxy/buz': secureCookiesProxy({
+        target: 'https://buz.example.com',
+        keychainAccount: 'example.com',
+      });
+  }
+}
+
+module.exports = config;
+```
+
+This will automatically proxy local requests for `http://locahost:$PORT/proxy/*` to `https://proxy-target.example.com`. If the server returns a `401` HTTP status code, the dev server will automatically prompt you to enter a cookie string, store it in system keychain, and use the cookies to authenticate future requests.
+
+## License
+
+The MIT License.
---
+++ src/secureCookieProxy.ts
@@ -0,0 +1,176 @@
+/**
+ * Add proxy to local mock server or production server.
+ */
+import { ClientRequest, IncomingMessage } from 'http';
+import { Config as ProxyOptions } from 'http-proxy-middleware';
+import { getPassword, setPassword } from 'keytar';
+import { parse as parseCookies, serialize as serializeCookie } from 'cookie';
+import * as inquirer from 'inquirer';
+
+const SERVICE = 'HttpProxySecureCookies';
+
+type Cookies = { [key: string]: string };
+
+function toCookieString(cookies: Cookies) {
+  return Object.entries(cookies)
+    .map(([key, value]) => serializeCookie(key, value))
+    .join('; ');
+}
+
+function parseSetCookie(
+  cookieHeaders?: string | number | string[],
+): [string[], Cookies] {
+  if (!cookieHeaders) return [[], {}];
+  const cookieArray = Array.isArray(cookieHeaders)
+    ? cookieHeaders
+    : [String(cookieHeaders)];
+  const cookies: Cookies = {};
+  return [
+    cookieArray,
+    cookieArray
+      .map((x) => x.split(';', 1)[0].split('=', 2))
+      .reduce((acc, [key, value]) => {
+        acc[key] = value;
+        return acc;
+      }, cookies),
+  ];
+}
+
+export interface SecureCookieProxyOptions extends ProxyOptions {
+  /**
+   * Target proxy destination.
+   */
+  target: string; // make proxy target a required option
+  /**
+   * HTTP status code indicating when auth cookies are needed (default: 401).
+   */
+  unauthorizedStatusCode?: number | number[];
+  /**
+   * The account name for the cookies saved in keychain.
+   */
+  keychainAccount?: string;
+  /**
+   * Additional processing on secure cookies before setting them to the client.
+   */
+  cookieRewrite?: (cookies: Cookies) => Cookies;
+}
+
+/**
+ * Generate a proxy config that points to a remote host.
+ */
+export default function secureCookieProxy(
+  options: string | SecureCookieProxyOptions,
+): ProxyOptions {
+  const {
+    target,
+    keychainAccount,
+    unauthorizedStatusCode = [401],
+    cookieRewrite,
+    ...restOptions
+  }: SecureCookieProxyOptions =
+    typeof options === 'string' ? { target: options } : options;
+  // default account is the target sans protocol
+  const account = keychainAccount || target.split('://')[1];
+  const unauthroizedCode = new Set(
+    Array.isArray(unauthorizedStatusCode)
+      ? unauthorizedStatusCode
+      : [unauthorizedStatusCode],
+  );
+
+  let secureCookies: Cookies | undefined;
+  let waitingForInput = false;
+
+  async function getFromKeyChain() {
+    return getPassword(SERVICE, account);
+  }
+
+  async function askForUserInput(message: string): Promise<string | null> {
+    waitingForInput = true;
+    const { cookieString } = await inquirer.prompt<{ cookieString: string }>([
+      {
+        type: 'password',
+        name: 'cookieString',
+        message,
+      },
+    ]);
+    await setPassword(SERVICE, account, cookieString);
+    waitingForInput = false;
+    console.log('\nSuccessfully saved your cookie. Please refresh.');
+    return cookieString;
+  }
+
+  async function getCookies(message?: string) {
+    if (waitingForInput) return null;
+    try {
+      secureCookies = parseCookies(
+        (message ? await askForUserInput(message) : await getFromKeyChain()) ||
+          '',
+      );
+    } catch {
+      await getCookies('Invalid cookie string, please try again: ');
+    }
+    return secureCookies;
+  }
+
+  /**
+   * Add secure cookies to proxy request header
+   */
+  function addCookie(proxyReq: ClientRequest, req: IncomingMessage) {
+    proxyReq.setHeader(
+      'cookie',
+      toCookieString({
+        ...secureCookies,
+        ...parseCookies(req.headers.cookie || ''),
+      }),
+    );
+  }
+
+  // quitely get initial cookies
+  getCookies();
+
+  return {
+    secure: false,
+    changeOrigin: true,
+    target,
+    ws: target.startsWith('ws'),
+    ...restOptions,
+    onProxyReq: addCookie,
+    onProxyReqWs: addCookie,
+    onProxyRes(proxyRes, req, res) {
+      // update cookies if API returns 401
+      if (proxyRes.statusCode && unauthroizedCode.has(proxyRes.statusCode)) {
+        getCookies(`
+Authentication failed for ${target}${req.url}
+
+You either haven't provide an auth cookie or it expired.
+Please login to ${target} and copy the HTTP cookie string here.
+
+It will be securely stored in system keychain:`);
+      }
+      // add missing cookies to the response so they can be used on the client
+      // side as well.
+      if (secureCookies) {
+        const reqCookies = parseCookies(req.headers.cookie || '');
+        const [cookieHeaders, resCookies] = parseSetCookie(
+          res.getHeader('set-cookie'),
+        );
+        const clientCookies = cookieRewrite
+          ? cookieRewrite(secureCookies)
+          : secureCookies;
+        let hasMissingCookie = false;
+        Object.keys(clientCookies).forEach((key) => {
+          if (!(key in reqCookies) && !(key in resCookies)) {
+            // set all secure cookie to the root path
+            cookieHeaders.push(
+              serializeCookie(key, clientCookies[key], { path: '/' }),
+            );
+            hasMissingCookie = true;
+          }
+        });
+        if (hasMissingCookie) {
+          res.setHeader('Set-Cookie', cookieHeaders);
+        }
+      }
+    },
+  };
+}
